28a967dd13adcd774fbc0c581fca351c7bf9418f
Renette Ros <supergirl0894+git@gmail.com>
2020-04-08 19:16:56 +0200

    Fix service loading and optimize imports

--- game-runner/src/main/java/za/co/entelect/challenge/engine/loader/GameEngineClassLoader.java
+++ game-runner/src/main/java/za/co/entelect/challenge/engine/loader/GameEngineClassLoader.java
@@ -1,21 +1,15 @@
 package za.co.entelect.challenge.engine.loader;
 
-import org.reflections.Reflections;
-
-import java.io.File;
-import java.lang.reflect.Method;
-import java.net.URL;
-import java.net.URLClassLoader;
 import java.util.Iterator;
-import java.util.Set;
 import java.util.ServiceLoader;
 
 public class GameEngineClassLoader {
-    public <T> T loadEngineObject(Class<T> cl) throws Exception {
+    public <T> T loadEngineObject(Class<T> cl) {
         final ServiceLoader<T> loader = ServiceLoader.load(cl);
         final Iterator<T> iterator = loader.iterator();
-        if (!iterator.hasNext())
-            throw new Exception("Couldn't find implementation for " + cl.toString() + " on the classpath.");
+        if (!iterator.hasNext()) {
+            throw new IllegalArgumentException("Couldn't find implementation for " + cl.toString() + " on the classpath.");
+        }
         return iterator.next();
     }
 }
